f4624f574b6cb2c56c385b0efe642a83b73a4be4
wangben <wangben-1203@qq.com>
2025-05-13 13:55:14 +0800

    m

---
+++ time_test.go
@@ -0,0 +1,74 @@
+package date_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/go-dev-pkg/date"
+)
+
+func TestWeekStartAndEnd(t *testing.T) {
+	// 跨年的周
+	times, err := time.ParseInLocation(time.DateTime, "2025-12-31 08:08:08", time.Local)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	start, end := date.WeekStartAndEnd(times)
+	if s := start.Format(time.DateTime); s != "2025-12-29 00:00:00" {
+		t.Fatal(s, "!=", "2025-12-29 00:00:00")
+		return
+	}
+	if e := end.Format(time.DateTime); e != "2026-01-04 23:59:59" {
+		t.Fatal(e, "!=", "2026-01-04 23:59:59")
+		return
+	}
+
+	// 跨年的周
+	times, err = time.ParseInLocation(time.DateTime, "2026-01-01 08:08:08", time.Local)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	start, end = date.WeekStartAndEnd(times)
+	if s := start.Format(time.DateTime); s != "2025-12-29 00:00:00" {
+		t.Fatal(s, "!=", "2025-12-29 00:00:00")
+		return
+	}
+	if e := end.Format(time.DateTime); e != "2026-01-04 23:59:59" {
+		t.Fatal(e, "!=", "2026-01-04 23:59:59")
+		return
+	}
+
+	// 周一
+	times, err = time.ParseInLocation(time.DateTime, "2025-12-01 08:08:08", time.Local)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	start, end = date.WeekStartAndEnd(times)
+	if s := start.Format(time.DateTime); s != "2025-12-01 00:00:00" {
+		t.Fatal(s, "!=", "2025-12-01 00:00:00")
+		return
+	}
+	if e := end.Format(time.DateTime); e != "2025-12-07 23:59:59" {
+		t.Fatal(e, "!=", "2025-12-07 23:59:59")
+		return
+	}
+
+	// 周日
+	times, err = time.ParseInLocation(time.DateTime, "2025-12-07 08:08:08", time.Local)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	start, end = date.WeekStartAndEnd(times)
+	if s := start.Format(time.DateTime); s != "2025-12-01 00:00:00" {
+		t.Fatal(s, "!=", "2025-12-01 00:00:00")
+		return
+	}
+	if e := end.Format(time.DateTime); e != "2025-12-07 23:59:59" {
+		t.Fatal(e, "!=", "2025-12-07 23:59:59")
+		return
+	}
+}
