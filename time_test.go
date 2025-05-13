7dd9be35c6ce00db238590cb8cfd137d8a457705
wangben <wangben-1203@qq.com>
2025-05-13 14:08:25 +0800

    time:month

--- time_test.go
+++ time_test.go
@@ -136,3 +136,37 @@ func TestWeekStartAndEnd(t *testing.T) {
 		return
 	}
 }
+
+func TestMonthStartAndEnd(t *testing.T) {
+	// 闰年
+	times, err := time.ParseInLocation(time.DateTime, "2024-02-12 08:08:08", time.Local)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	start, end := date.MonthStartAndEnd(times)
+	if s := start.Format(time.DateTime); s != "2024-02-01 00:00:00" {
+		t.Fatal(s, "!=", "2024-02-01 00:00:00")
+		return
+	}
+	if e := end.Format(time.DateTime); e != "2024-02-29 23:59:59" {
+		t.Fatal(e, "!=", "2024-02-29 23:59:59")
+		return
+	}
+
+	// 平年
+	times, err = time.ParseInLocation(time.DateTime, "2025-02-12 08:08:08", time.Local)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	start, end = date.MonthStartAndEnd(times)
+	if s := start.Format(time.DateTime); s != "2025-02-01 00:00:00" {
+		t.Fatal(s, "!=", "2025-02-01 00:00:00")
+		return
+	}
+	if e := end.Format(time.DateTime); e != "2025-02-28 23:59:59" {
+		t.Fatal(e, "!=", "2025-02-28 23:59:59")
+		return
+	}
+}
