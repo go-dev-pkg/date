57ec68a8189107bf1f5fedeb1379594bdb603447
wangben <wangben-1203@qq.com>
2025-05-13 13:59:42 +0800

    m

--- time_test.go
+++ time_test.go
@@ -71,4 +71,36 @@ func TestWeekStartAndEnd(t *testing.T) {
 		t.Fatal(e, "!=", "2025-12-07 23:59:59")
 		return
 	}
+
+	// 平年跨月的周
+	times, err = time.ParseInLocation(time.DateTime, "2025-02-27 08:08:08", time.Local)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	start, end = date.WeekStartAndEnd(times)
+	if s := start.Format(time.DateTime); s != "2025-02-24 00:00:00" {
+		t.Fatal(s, "!=", "2025-02-24 00:00:00")
+		return
+	}
+	if e := end.Format(time.DateTime); e != "2025-03-02 23:59:59" {
+		t.Fatal(e, "!=", "2025-03-02 23:59:59")
+		return
+	}
+
+	// 平年跨月的周
+	times, err = time.ParseInLocation(time.DateTime, "2025-03-02 08:08:08", time.Local)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	start, end = date.WeekStartAndEnd(times)
+	if s := start.Format(time.DateTime); s != "2025-02-24 00:00:00" {
+		t.Fatal(s, "!=", "2025-02-24 00:00:00")
+		return
+	}
+	if e := end.Format(time.DateTime); e != "2025-03-02 23:59:59" {
+		t.Fatal(e, "!=", "2025-03-02 23:59:59")
+		return
+	}
 }
