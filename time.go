c9a4566cece1274b6528c65bb84c110d1a3ea6da
wangben <wangben-1203@qq.com>
2025-05-13 11:49:39 +0800

    time

--- time.go
+++ time.go
@@ -4,6 +4,7 @@ import (
 	"time"
 )
 
+// DayStartAndEnd 当天的开始和结束时间
 func DayStartAndEnd(t ...time.Time) (start, end time.Time) {
 	nt := time.Now()
 	if len(t) > 0 && !t[0].IsZero() {
@@ -14,6 +15,7 @@ func DayStartAndEnd(t ...time.Time) (start, end time.Time) {
 	return
 }
 
+// MonthStartAndEnd 当月的开始和结束时间
 func MonthStartAndEnd(t ...time.Time) (start, end time.Time) {
 	nt := time.Now()
 	if len(t) > 0 && !t[0].IsZero() {
@@ -24,6 +26,37 @@ func MonthStartAndEnd(t ...time.Time) (start, end time.Time) {
 	return
 }
 
+// QuarterStartAndEnd 当季度的开始和结束时间
+func QuarterStartAndEnd(t ...time.Time) (start, end time.Time) {
+	nt := time.Now()
+	if len(t) > 0 && !t[0].IsZero() {
+		nt = t[0]
+	}
+	month := int(nt.Month())
+	if month == 1 || month == 2 || month == 3 {
+		start = time.Date(nt.Year(), 1, 1, 0, 0, 0, 0, time.Local)
+		end = time.Date(nt.Year(), 3, 31, 23, 59, 59, 0, time.Local)
+		return
+	}
+	if month == 4 || month == 5 || month == 6 {
+		start = time.Date(nt.Year(), 4, 1, 0, 0, 0, 0, time.Local)
+		end = time.Date(nt.Year(), 6, 30, 23, 59, 59, 0, time.Local)
+		return
+	}
+	if month == 7 || month == 8 || month == 9 {
+		start = time.Date(nt.Year(), 7, 1, 0, 0, 0, 0, time.Local)
+		end = time.Date(nt.Year(), 9, 30, 23, 59, 59, 0, time.Local)
+		return
+	}
+	if month == 10 || month == 11 || month == 12 {
+		start = time.Date(nt.Year(), 10, 1, 0, 0, 0, 0, time.Local)
+		end = time.Date(nt.Year(), 12, 31, 23, 59, 59, 0, time.Local)
+		return
+	}
+	return
+}
+
+// YearStartAndEnd 当年的开始和结束时间
 func YearStartAndEnd(t ...time.Time) (start, end time.Time) {
 	nt := time.Now()
 	if len(t) > 0 && !t[0].IsZero() {
